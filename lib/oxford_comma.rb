9f534f72899b914ef8c1e9e5b14370b4ce8186c7
Estefano Livi <estefanolivi@outlook.com>
2020-07-14 20:32:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,14 @@
 def oxford_comma(array)
   if array.length == 1
-    array.join
+    array.join(",")
   elsif array.length == 2
-    array.join(" and ")
-  elsif array.length == 3
-   array[-1]=" and #{array} [-1]"
-    array.join (" "  "and" )
-  end
+    array[-2].insert(0, "and ")
+    array.join(" ")
+  elsif array.length >= 3
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  else  
+    array.join
+  end  
 end
 
